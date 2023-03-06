567bb450583762cf252662bc262ec8b4344c8173
sotlet <sophie.otlet@student.uclouvain.be>
2023-03-06 17:01:27 +0100

    srdc windows and object + attribute parents for sublocks

--- Melodizer-main/Melodizer/sources/rock.lisp
+++ Melodizer-main/Melodizer/sources/rock.lisp
@@ -15,15 +15,13 @@
 (om::defclass! rock ()
     (
     (block-list :accessor block-list :initarg :block-list :initform nil :documentation "")
-    (object-A :accessor object-A :initarg :object-A :initform nil :documentation "")
+    (A-first :accessor A-first :initarg :A-first :initform nil :documentation "")
+    (A-second :accessor A-second :initarg :A-second :initform nil :documentation "")
     (object-B :accessor object-B :initarg :object-B :initform nil :documentation "")
+    (A-third :accessor A-third :initarg :A-third :initform nil :documentation "")
     )
 )
 
-;; (om::defclass! ablock ()
-;;     ((block-list :accessor block-list :initarg :block-list :initform nil :documentation "")
-;;     )
-;; )
 
 (defclass rock-editor (om::editorview) ())
 
@@ -84,7 +82,12 @@
 
 
 (om::defclass! A ()
-    ((block-list :accessor block-list :initarg :block-list :initform nil :documentation "")
+    (
+      (s-block :accessor s-block :initarg :s-block :initform nil :documentation "")
+      (r-block :accessor r-block :initarg :r-block :initform nil :documentation "")
+      (d-block :accessor d-block :initarg :d-block :initform nil :documentation "")
+      (c-block :accessor c-block :initarg :c-block :initform nil :documentation "")
+      (parent :accessor parent :initarg :parent :initform nil :documentation "")
     )
 )
 
@@ -148,9 +151,13 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-
 (om::defclass! B ()
-    ((block-list :accessor block-list :initarg :block-list :initform nil :documentation "")
+    (
+      (s-block :accessor s-block :initarg :s-block :initform nil :documentation "")
+      (r-block :accessor r-block :initarg :r-block :initform nil :documentation "")
+      (d-block :accessor d-block :initarg :d-block :initform nil :documentation "")
+      (c-block :accessor c-block :initarg :c-block :initform nil :documentation "")
+      (parent :accessor parent :initarg :parent :initform nil :documentation "")
     )
 )
 
@@ -210,6 +217,261 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                          s CLASS                            ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(om::defclass! s ()
+    ((block-list :accessor block-list :initarg :block-list :initform nil :documentation "")
+    (parent :accessor parent :initarg :parent :initform nil :documentation "")
+    )
+)
+
+(defclass s-editor (om::editorview) ())
+
+(defmethod om::class-has-editor-p ((self s)) t)
+(defmethod om::get-editor-class ((self s)) 's-editor)
+
+(defmethod om::om-draw-contents ((view s-editor))
+  (let* ((object (om::object view)))
+    (om::om-with-focused-view
+      view
+      ;;; DRAW SOMETHING ?
+    )
+  )
+)
+
+(defmethod initialize-instance ((self s-editor) &rest args)
+  ;;; do what needs to be done by default
+  (call-next-method) ; start the search by default?
+  (make-my-interface self)
+)
+
+
+(defmethod make-my-interface ((self s-editor))
+
+  ; create the main view of the object
+  (make-main-view self)
+
+  (let*
+    (
+      ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+      ;;; setting the different regions of the tool ;;;
+      ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+      (s-panel (om::om-make-view 'om::om-view
+        :size (om::om-make-point 600 150)
+        :position (om::om-make-point 5 5)
+        :bg-color om::*azulito*)
+      )
+    )
+
+    (setf elements-s-panel (make-s-panel self s-panel))
+
+    ; add the subviews for the different parts into the main view
+    (om::om-add-subviews
+      self
+      s-panel
+    )
+  )
+  ; return the editor
+  self
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                          r CLASS                            ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(om::defclass! r ()
+    ((block-list :accessor block-list :initarg :block-list :initform nil :documentation "")
+    (parent :accessor parent :initarg :parent :initform nil :documentation "")
+    )
+)
+
+(defclass r-editor (om::editorview) ())
+
+(defmethod om::class-has-editor-p ((self r)) t)
+(defmethod om::get-editor-class ((self r)) 'r-editor)
+
+(defmethod om::om-draw-contents ((view r-editor))
+  (let* ((object (om::object view)))
+    (om::om-with-focused-view
+      view
+      ;;; DRAW SOMETHING ?
+    )
+  )
+)
+
+(defmethod initialize-instance ((self r-editor) &rest args)
+  ;;; do what needs to be done by default
+  (call-next-method) ; start the search by default?
+  (make-my-interface self)
+)
+
+
+(defmethod make-my-interface ((self r-editor))
+
+  ; create the main view of the object
+  (make-main-view self)
+
+  (let*
+    (
+      ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+      ;;; setting the different regions of the tool ;;;
+      ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+      (r-panel (om::om-make-view 'om::om-view
+        :size (om::om-make-point 600 150)
+        :position (om::om-make-point 5 5)
+        :bg-color om::*azulito*)
+      )
+    )
+
+    (setf elements-r-panel (make-r-panel self r-panel))
+
+    ; add the subviews for the different parts into the main view
+    (om::om-add-subviews
+      self
+      r-panel
+    )
+  )
+  ; return the editor
+  self
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                          d CLASS                            ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(om::defclass! d ()
+    ((block-list :accessor block-list :initarg :block-list :initform nil :documentation "")
+    (parent :accessor parent :initarg :parent :initform nil :documentation "")
+    )
+)
+
+(defclass d-editor (om::editorview) ())
+
+(defmethod om::class-has-editor-p ((self d)) t)
+(defmethod om::get-editor-class ((self d)) 'd-editor)
+
+(defmethod om::om-draw-contents ((view d-editor))
+  (let* ((object (om::object view)))
+    (om::om-with-focused-view
+      view
+      ;;; DRAW SOMETHING ?
+    )
+  )
+)
+
+(defmethod initialize-instance ((self d-editor) &rest args)
+  ;;; do what needs to be done by default
+  (call-next-method) ; start the search by default?
+  (make-my-interface self)
+)
+
+(defmethod make-my-interface ((self d-editor))
+
+  ; create the main view of the object
+  (make-main-view self)
+
+  (let*
+    (
+      ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+      ;;; setting the different regions of the tool ;;;
+      ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+      (d-panel (om::om-make-view 'om::om-view
+        :size (om::om-make-point 600 150)
+        :position (om::om-make-point 5 5)
+        :bg-color om::*azulito*)
+      )
+    )
+
+    (setf elements-d-panel (make-d-panel self d-panel))
+
+    ; add the subviews for the different parts into the main view
+    (om::om-add-subviews
+      self
+      d-panel
+    )
+  )
+  ; return the editor
+  self
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                          c CLASS                            ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(om::defclass! c ()
+    ((block-list :accessor block-list :initarg :block-list :initform nil :documentation "")
+    (parent :accessor parent :initarg :parent :initform nil :documentation "")
+    )
+)
+
+(defclass c-editor (om::editorview) ())
+
+(defmethod om::class-has-editor-p ((self c)) t)
+(defmethod om::get-editor-class ((self c)) 'c-editor)
+
+(defmethod om::om-draw-contents ((view c-editor))
+  (let* ((object (om::object view)))
+    (om::om-with-focused-view
+      view
+      ;;; DRAW SOMETHING ?
+    )
+  )
+)
+
+(defmethod initialize-instance ((self c-editor) &rest args)
+  ;;; do what needs to be done by default
+  (call-next-method) ; start the search by default?
+  (make-my-interface self)
+)
+
+(defmethod make-my-interface ((self c-editor))
+
+  ; create the main view of the object
+  (make-main-view self)
+
+  (let*
+    (
+      ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+      ;;; setting the different regions of the tool ;;;
+      ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+      (B-panel (om::om-make-view 'om::om-view
+        :size (om::om-make-point 600 150)
+        :position (om::om-make-point 5 5)
+        :bg-color om::*azulito*)
+      )
+    )
+
+    (setf elements-c-panel (make-c-panel self c-panel))
+
+    ; add the subviews for the different parts into the main view
+    (om::om-add-subviews
+      self
+      c-panel
+    )
+  )
+  ; return the editor
+  self
+)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;                  INTERFACE CONSTRUCTION                     ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -231,8 +493,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-
-
 (defun make-rock-panel (editor rock-panel)
   (om::om-add-subviews
     rock-panel
@@ -250,10 +510,9 @@
             "next thread" ; name of the thread, not necessary but useful for debugging
             nil ; process initialization keywords, not needed here
             (lambda () ; function to call
-              ;; (setf (object-A (om::object editor)) (make-instance 'A-editor (om::object editor)))
-              (setf (object-A (om::object editor)) (make-instance 'A (om::object editor)))
-              (om::openeditorframe ; open a voice window displaying the solution
-                (om::omNG-make-new-instance (object-A (om::object editor)) "Window A1")
+              (setf (A-first (om::object editor)) (make-instance 'A :parent editor (om::object editor)))
+              (om::openeditorframe ; open a window displaying the editor of the first A block
+                (om::omNG-make-new-instance (A-first (om::object editor)) "Window A1")
               )
             )
           )
@@ -274,10 +533,9 @@
             "next thread" ; name of the thread, not necessary but useful for debugging
             nil ; process initialization keywords, not needed here
             (lambda () ; function to call
-              ;; (setf (object-A (om::object editor)) (make-instance 'A-editor (om::object editor)))
-              (setf (object-A (om::object editor)) (make-instance 'A (om::object editor)))
+              (setf (A-second (om::object editor)) (make-instance 'A :parent editor (om::object editor)))
               (om::openeditorframe ; open a voice window displaying the solution
-                (om::omNG-make-new-instance (object-A (om::object editor)) "Window A2")
+                (om::omNG-make-new-instance (A-second (om::object editor)) "Window A2")
               )
             )
           )
@@ -296,8 +554,7 @@
             "next thread" ; name of the thread, not necessary but useful for debugging
             nil ; process initialization keywords, not needed here
             (lambda () ; function to call
-              ;; (setf (object-A (om::object editor)) (make-instance 'A-editor (om::object editor)))
-              (setf (object-B (om::object editor)) (make-instance 'B (om::object editor)))
+              (setf (object-B (om::object editor)) (make-instance 'B :parent editor (om::object editor)))
               (om::openeditorframe ; open a voice window displaying the solution
                 (om::omNG-make-new-instance (object-B (om::object editor)) "Window B")
               )
@@ -318,10 +575,9 @@
             "next thread" ; name of the thread, not necessary but useful for debugging
             nil ; process initialization keywords, not needed here
             (lambda () ; function to call
-              ;; (setf (object-A (om::object editor)) (make-instance 'A-editor (om::object editor)))
-              (setf (object-A (om::object editor)) (make-instance 'A (om::object editor)))
+              (setf (A-third (om::object editor)) (make-instance 'A :parent editor (om::object editor)))
               (om::openeditorframe ; open a voice window displaying the solution
-                (om::omNG-make-new-instance (object-A (om::object editor)) "Window A3")
+                (om::omNG-make-new-instance (A-third (om::object editor)) "Window A3")
               )
             )
           )
@@ -351,6 +607,16 @@
         "s"
         :di-action #'(lambda (b)
           (print "Selected s")
+          (mp:process-run-function ; start a new thread for the execution of the next method
+            "next thread" ; name of the thread, not necessary but useful for debugging
+            nil ; process initialization keywords, not needed here
+            (lambda () ; function to call
+              (setf (s-block (om::object editor)) (make-instance 's :parent editor (om::object editor)))
+              (om::openeditorframe ; open a window displaying the editor of the first A block
+                (om::omNG-make-new-instance (s-block (om::object editor)) "Window s")
+              )
+            )
+          )
         )
     )
     (om::om-make-dialog-item
@@ -360,6 +626,16 @@
         "r"
         :di-action #'(lambda (b)
           (print "Selected r")
+          (mp:process-run-function ; start a new thread for the execution of the next method
+            "next thread" ; name of the thread, not necessary but useful for debugging
+            nil ; process initialization keywords, not needed here
+            (lambda () ; function to call
+              (setf (r-block (om::object editor)) (make-instance 'r :parent editor (om::object editor)))
+              (om::openeditorframe ; open a window displaying the editor of the first A block
+                (om::omNG-make-new-instance (r-block (om::object editor)) "Window r")
+              )
+            )
+          )
         )
     )
     (om::om-make-dialog-item
@@ -369,6 +645,16 @@
         "d"
         :di-action #'(lambda (b)
           (print "Selected d")
+          (mp:process-run-function ; start a new thread for the execution of the next method
+            "next thread" ; name of the thread, not necessary but useful for debugging
+            nil ; process initialization keywords, not needed here
+            (lambda () ; function to call
+              (setf (d-block (om::object editor)) (make-instance 'd :parent editor (om::object editor)))
+              (om::openeditorframe ; open a window displaying the editor of the first A block
+                (om::omNG-make-new-instance (d-block (om::object editor)) "Window d")
+              )
+            )
+          )
         )
     )
     (om::om-make-dialog-item
@@ -378,6 +664,16 @@
         "c"
         :di-action #'(lambda (b)
           (print "Selected c")
+          (mp:process-run-function ; start a new thread for the execution of the next method
+            "next thread" ; name of the thread, not necessary but useful for debugging
+            nil ; process initialization keywords, not needed here
+            (lambda () ; function to call
+              (setf (c-block (om::object editor)) (make-instance 'c :parent editor (om::object editor)))
+              (om::openeditorframe ; open a window displaying the editor of the first A block
+                (om::omNG-make-new-instance (c-block (om::object editor)) "Window c")
+              )
+            )
+          )
         )
     )
   )
@@ -402,6 +698,16 @@
         "s"
         :di-action #'(lambda (b)
           (print "Selected s")
+          (mp:process-run-function ; start a new thread for the execution of the next method
+            "next thread" ; name of the thread, not necessary but useful for debugging
+            nil ; process initialization keywords, not needed here
+            (lambda () ; function to call
+              (setf (s-block (om::object editor)) (make-instance 's :parent editor (om::object editor)))
+              (om::openeditorframe ; open a window displaying the editor of the first A block
+                (om::omNG-make-new-instance (s-block (om::object editor)) "Window s")
+              )
+            )
+          )
         )
     )
     (om::om-make-dialog-item
@@ -411,6 +717,16 @@
         "r"
         :di-action #'(lambda (b)
           (print "Selected r")
+          (mp:process-run-function ; start a new thread for the execution of the next method
+            "next thread" ; name of the thread, not necessary but useful for debugging
+            nil ; process initialization keywords, not needed here
+            (lambda () ; function to call
+              (setf (r-block (om::object editor)) (make-instance 'r :parent editor (om::object editor)))
+              (om::openeditorframe ; open a window displaying the editor of the first A block
+                (om::omNG-make-new-instance (r-block (om::object editor)) "Window r")
+              )
+            )
+          )
         )
     )
     (om::om-make-dialog-item
@@ -420,6 +736,16 @@
         "d"
         :di-action #'(lambda (b)
           (print "Selected d")
+          (mp:process-run-function ; start a new thread for the execution of the next method
+            "next thread" ; name of the thread, not necessary but useful for debugging
+            nil ; process initialization keywords, not needed here
+            (lambda () ; function to call
+              (setf (d-block (om::object editor)) (make-instance 'd :parent editor (om::object editor)))
+              (om::openeditorframe ; open a window displaying the editor of the first A block
+                (om::omNG-make-new-instance (d-block (om::object editor)) "Window d")
+              )
+            )
+          )
         )
     )
     (om::om-make-dialog-item
@@ -429,8 +755,34 @@
         "c"
         :di-action #'(lambda (b)
           (print "Selected c")
+          (mp:process-run-function ; start a new thread for the execution of the next method
+            "next thread" ; name of the thread, not necessary but useful for debugging
+            nil ; process initialization keywords, not needed here
+            (lambda () ; function to call
+              (setf (c-block (om::object editor)) (make-instance 'c :parent editor (om::object editor)))
+              (om::openeditorframe ; open a window displaying the editor of the first A block
+                (om::omNG-make-new-instance (c-block (om::object editor)) "Window c")
+              )
+            )
+          )
         )
     )
   )
+)
+
+(defun make-s-panel (editor s-panel)
+
+)
+
+(defun make-r-panel (editor r-panel)
+
+)
+
+(defun make-d-panel (editor d-panel)
+
+)
+
+(defun make-c-panel (editor c-panel)
+
 )
 
