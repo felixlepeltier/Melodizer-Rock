f87c934783daadf995a29236555e0cc9c8554567
sotlet <sophie.otlet@student.uclouvain.be>
2023-03-30 10:53:21 +0200

    separation AB srdc from rock

---
+++ Melodizer-main/Melodizer/sources/rock-srdc.lisp
@@ -0,0 +1,326 @@
+(in-package :mldz)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                          s CLASS                            ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(om::defclass! s ()
+    (
+      (parent :accessor parent :initarg :parent :initform nil :documentation "")
+      (melody-source :accessor melody-source :initarg :melody-source :initform nil :documentation "")
+      (bar-length :accessor bar-length :initform 0 :type integer)
+      (min-pushed-notes :accessor min-pushed-notes :initform nil :type integer)
+      (max-pushed-notes :accessor max-pushed-notes :initform nil :type integer)
+      (min-notes :accessor min-notes :initform nil :type integer)
+      (max-notes :accessor max-notes :initform nil :type integer)
+      (min-note-length-flag :accessor min-note-length-flag :initform nil :type integer)
+      (min-note-length :accessor min-note-length :initform 0 :type integer)
+      (max-note-length-flag :accessor max-note-length-flag :initform nil :type integer)
+      (max-note-length :accessor max-note-length :initform 192 :type integer)
+      (quantification :accessor quantification :initform nil :type string)
+      (key-selection :accessor key-selection :initform nil :type string)
+      (mode-selection :accessor mode-selection :initform nil :type string)
+      (chord-key :accessor chord-key :initform nil :type string)
+      (chord-quality :accessor chord-quality :initform nil :type string)
+      (min-pitch :accessor min-pitch :initform 1 :type integer)
+      (min-pitch-flag :accessor min-pitch-flag :initform nil :type integer)
+      (max-pitch :accessor max-pitch :initform 127 :type integer)
+      (max-pitch-flag :accessor max-pitch-flag :initform nil :type integer)
+    )
+)
+
+(defclass s-editor (om::editorview) ())
+
+(defmethod om::class-has-editor-p ((self s)) t)
+(defmethod om::get-editor-class ((self s)) 's-editor)
+
+(defmethod om::om-draw-contents ((view s-editor))
+  (let* ((object (om::object view)))
+    (om::om-with-focused-view
+      view
+      ;;; DRAW SOMETHING ?
+    )
+  )
+)
+
+(defmethod initialize-instance ((self s-editor) &rest args)
+  ;;; do what needs to be done by default
+  (call-next-method) ; start the search by default?
+  (make-my-interface self)
+)
+
+
+(defmethod make-my-interface ((self s-editor))
+
+  ; create the main view of the object
+  (make-main-view self)
+
+  (let*
+    (
+      ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+      ;;; setting the different regions of the tool ;;;
+      ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+       (constraints-panel (om::om-make-view 'om::om-view
+        :size (om::om-make-point 810 500)
+        :position (om::om-make-point 5 5)
+        :bg-color om::*azulito*)
+      )
+    )
+
+    (setf elements-constraints-panel (make-constraints-panel self constraints-panel))
+
+    ; add the subviews for the different parts into the main view
+    (om::om-add-subviews
+      self
+      constraints-panel
+    )
+  )
+  ; return the editor
+  self
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                          r CLASS                            ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(om::defclass! r ()
+    (
+      (parent :accessor parent :initarg :parent :initform nil :documentation "")
+      (melody-source :accessor melody-source :initarg :melody-source :initform nil :documentation "")
+      (bar-length :accessor bar-length :initform 0 :type integer)
+      (min-pushed-notes :accessor min-pushed-notes :initform nil :type integer)
+      (max-pushed-notes :accessor max-pushed-notes :initform nil :type integer)
+      (min-notes :accessor min-notes :initform nil :type integer)
+      (max-notes :accessor max-notes :initform nil :type integer)
+      (min-note-length-flag :accessor min-note-length-flag :initform nil :type integer)
+      (min-note-length :accessor min-note-length :initform 0 :type integer)
+      (max-note-length-flag :accessor max-note-length-flag :initform nil :type integer)
+      (max-note-length :accessor max-note-length :initform 192 :type integer)
+      (quantification :accessor quantification :initform nil :type string)
+      (key-selection :accessor key-selection :initform nil :type string)
+      (mode-selection :accessor mode-selection :initform nil :type string)
+      (chord-key :accessor chord-key :initform nil :type string)
+      (chord-quality :accessor chord-quality :initform nil :type string)
+      (min-pitch :accessor min-pitch :initform 1 :type integer)
+      (min-pitch-flag :accessor min-pitch-flag :initform nil :type integer)
+      (max-pitch :accessor max-pitch :initform 127 :type integer)
+      (max-pitch-flag :accessor max-pitch-flag :initform nil :type integer)
+    )
+)
+
+(defclass r-editor (om::editorview) ())
+
+(defmethod om::class-has-editor-p ((self r)) t)
+(defmethod om::get-editor-class ((self r)) 'r-editor)
+
+(defmethod om::om-draw-contents ((view r-editor))
+  (let* ((object (om::object view)))
+    (om::om-with-focused-view
+      view
+      ;;; DRAW SOMETHING ?
+    )
+  )
+)
+
+(defmethod initialize-instance ((self r-editor) &rest args)
+  ;;; do what needs to be done by default
+  (call-next-method) ; start the search by default?
+  (make-my-interface self)
+)
+
+
+(defmethod make-my-interface ((self r-editor))
+
+  ; create the main view of the object
+  (make-main-view self)
+
+  (let*
+    (
+      ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+      ;;; setting the different regions of the tool ;;;
+      ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+       (constraints-panel (om::om-make-view 'om::om-view
+        :size (om::om-make-point 810 500)
+        :position (om::om-make-point 5 5)
+        :bg-color om::*azulito*)
+      )
+    )
+
+    (setf elements-constraints-panel (make-constraints-panel self constraints-panel))
+
+    ; add the subviews for the different parts into the main view
+    (om::om-add-subviews
+      self
+      constraints-panel
+    )
+  )
+  ; return the editor
+  self
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                          d CLASS                            ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(om::defclass! d ()
+    (
+      (melody-source :accessor melody-source :initarg :melody-source :initform nil :documentation "")
+      (bar-length :accessor bar-length :initform 0 :type integer)
+      (min-pushed-notes :accessor min-pushed-notes :initform nil :type integer)
+      (max-pushed-notes :accessor max-pushed-notes :initform nil :type integer)
+      (min-notes :accessor min-notes :initform nil :type integer)
+      (max-notes :accessor max-notes :initform nil :type integer)
+      (min-note-length-flag :accessor min-note-length-flag :initform nil :type integer)
+      (min-note-length :accessor min-note-length :initform 0 :type integer)
+      (max-note-length-flag :accessor max-note-length-flag :initform nil :type integer)
+      (max-note-length :accessor max-note-length :initform 192 :type integer)
+      (quantification :accessor quantification :initform nil :type string)
+      (key-selection :accessor key-selection :initform nil :type string)
+      (mode-selection :accessor mode-selection :initform nil :type string)
+      (chord-key :accessor chord-key :initform nil :type string)
+      (chord-quality :accessor chord-quality :initform nil :type string)
+      (min-pitch :accessor min-pitch :initform 1 :type integer)
+      (min-pitch-flag :accessor min-pitch-flag :initform nil :type integer)
+      (max-pitch :accessor max-pitch :initform 127 :type integer)
+      (max-pitch-flag :accessor max-pitch-flag :initform nil :type integer)
+    )
+)
+
+(defclass d-editor (om::editorview) ())
+
+(defmethod om::class-has-editor-p ((self d)) t)
+(defmethod om::get-editor-class ((self d)) 'd-editor)
+
+(defmethod om::om-draw-contents ((view d-editor))
+  (let* ((object (om::object view)))
+    (om::om-with-focused-view
+      view
+      ;;; DRAW SOMETHING ?
+    )
+  )
+)
+
+(defmethod initialize-instance ((self d-editor) &rest args)
+  ;;; do what needs to be done by default
+  (call-next-method) ; start the search by default?
+  (make-my-interface self)
+)
+
+(defmethod make-my-interface ((self d-editor))
+
+  ; create the main view of the object
+  (make-main-view self)
+
+  (let*
+    (
+      ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+      ;;; setting the different regions of the tool ;;;
+      ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+       (constraints-panel (om::om-make-view 'om::om-view
+        :size (om::om-make-point 810 500)
+        :position (om::om-make-point 5 5)
+        :bg-color om::*azulito*)
+      )
+    )
+
+    (setf elements-constraints-panel (make-constraints-panel self constraints-panel))
+
+    ; add the subviews for the different parts into the main view
+    (om::om-add-subviews
+      self
+      constraints-panel
+    )
+  )
+  ; return the editor
+  self
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                          c CLASS                            ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(om::defclass! c ()
+    (
+      (melody-source :accessor melody-source :initarg :melody-source :initform nil :documentation "")
+      (bar-length :accessor bar-length :initform 0 :type integer)
+      (min-pushed-notes :accessor min-pushed-notes :initform nil :type integer)
+      (max-pushed-notes :accessor max-pushed-notes :initform nil :type integer)
+      (min-notes :accessor min-notes :initform nil :type integer)
+      (max-notes :accessor max-notes :initform nil :type integer)
+      (min-note-length-flag :accessor min-note-length-flag :initform nil :type integer)
+      (min-note-length :accessor min-note-length :initform 0 :type integer)
+      (max-note-length-flag :accessor max-note-length-flag :initform nil :type integer)
+      (max-note-length :accessor max-note-length :initform 192 :type integer)
+      (quantification :accessor quantification :initform nil :type string)
+      (key-selection :accessor key-selection :initform nil :type string)
+      (mode-selection :accessor mode-selection :initform nil :type string)
+      (chord-key :accessor chord-key :initform nil :type string)
+      (chord-quality :accessor chord-quality :initform nil :type string)
+      (min-pitch :accessor min-pitch :initform 1 :type integer)
+      (min-pitch-flag :accessor min-pitch-flag :initform nil :type integer)
+      (max-pitch :accessor max-pitch :initform 127 :type integer)
+      (max-pitch-flag :accessor max-pitch-flag :initform nil :type integer)
+    )
+)
+
+(defclass c-editor (om::editorview) ())
+
+(defmethod om::class-has-editor-p ((self c)) t)
+(defmethod om::get-editor-class ((self c)) 'c-editor)
+
+(defmethod om::om-draw-contents ((view c-editor))
+  (let* ((object (om::object view)))
+    (om::om-with-focused-view
+      view
+      ;;; DRAW SOMETHING ?
+    )
+  )
+)
+
+(defmethod initialize-instance ((self c-editor) &rest args)
+  ;;; do what needs to be done by default
+  (call-next-method) ; start the search by default?
+  (make-my-interface self)
+)
+
+(defmethod make-my-interface ((self c-editor))
+
+  ; create the main view of the object
+  (make-main-view self)
+
+  (let*
+    (
+      ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+      ;;; setting the different regions of the tool ;;;
+      ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+       (constraints-panel (om::om-make-view 'om::om-view
+        :size (om::om-make-point 810 500)
+        :position (om::om-make-point 5 5)
+        :bg-color om::*azulito*)
+      )
+    )
+
+    (setf elements-constraints-panel (make-constraints-panel self constraints-panel))
+
+    ; add the subviews for the different parts into the main view
+    (om::om-add-subviews
+      self
+      constraints-panel
+    )
+  )
+  ; return the editor
+  self
+)
